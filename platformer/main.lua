aaed4ab7980a386c95bad1992d1a884db59d5361
robertcurry0216 <robertcurry0216@gmail.com>
2021-07-28 21:10:55 +1000

    drawing blocks

--- platformer/main.lua
+++ platformer/main.lua
@@ -1,6 +1,7 @@
 function love.load()
   tiles = love.graphics.newImage("assets/1bit_tileset.png")
-  frames = {}
+  
+  block = love.graphics.newQuad(48, 32, 16, 16, tiles)
 
   tilemap = {
     {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -19,7 +20,8 @@ function love.load()
     {1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1},
     {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
   }
-
+  
+  frames = {}
   table.insert( frames, love.graphics.newQuad(96, 32, 16, 16, tiles:getWidth(), tiles:getHeight()) )
   table.insert( frames, love.graphics.newQuad(96, 48, 16, 16, tiles:getWidth(), tiles:getHeight()) )
 
@@ -27,6 +29,7 @@ function love.load()
 end
 
 function love.update( dt )
+  print(dt)
   currentFrame = currentFrame + dt * 5
   if currentFrame >= 3 then
     currentFrame = 1
@@ -37,8 +40,8 @@ function love.draw()
   for y=1,#tilemap do
     for x=1,#tilemap[y] do
       if tilemap[y][x] == 1 then
-        love.graphics.rectangle(
-          "line", (x-1)*16, (y-1)*16, 16, 16
+        love.graphics.draw(
+          tiles, block, (x-1)*16, (y-1)*16
         )
       end
     end
