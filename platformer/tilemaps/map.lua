bd40002a083c949892ac5be6205c64a2a8da436a
robertcurry0216 <robertcurry0216@gmail.com>
2021-08-01 10:42:49 +1000

    tilemap drawing

--- platformer/tilemaps/map.lua
+++ platformer/tilemaps/map.lua
@@ -15,7 +15,28 @@ return {
     {
       name = "basic platformer",
       firstgid = 1,
-      filename = "basic platformer.tsx"
+      tilewidth = 16,
+      tileheight = 16,
+      spacing = 0,
+      margin = 0,
+      columns = 7,
+      image = "../assets/1bit_tileset.png",
+      imagewidth = 112,
+      imageheight = 112,
+      objectalignment = "unspecified",
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 16,
+        height = 16
+      },
+      properties = {},
+      wangsets = {},
+      tilecount = 49,
+      tiles = {}
     }
   },
   layers = {
--- platformer/tilesmaps/basic platformer.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.1" name="basic platformer" tilewidth="16" tileheight="16" tilecount="49" columns="7">
- <image source="../assets/1bit_tileset.png" width="112" height="112"/>
-</tileset>
